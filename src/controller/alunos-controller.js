dc5b7a30237d93ed586a1200c904fb72acc5cac7
yasminreisk <yasmin.reiss16@gmail.com>
2021-11-29 18:14:33 -0300

    Modificação no Controller

--- src/controller/alunos-controller.js
+++ src/controller/alunos-controller.js
@@ -1,107 +1,89 @@
 const alunosDAO = require('../DAO/alunos-DAO')
 const Alunos = require('../model/alunos-model')
-const db = require ('../infra/banco')
 
-module.exports = (app) => {
+const expAlunos = (app, db) => {
     const alunosDB = new alunosDAO(db)
 
-    app.get('/aluno', async (req, res) => {
+    app.get('/alunos', async (req, res) => {
         try {
             const resposta = await alunosDB.getAllAlunos()
-            res.status(200).json ({
-                result: resposta,
-                count: resposta.length
-            })
-        } catch (erro) {
-            res.status(400).json ({
-                message: erro.message,
-                error: true
-            })
-        }
-    })
+            res.json(resposta)
+    }
+        catch (error) {
+            res.json(error)
+    }
+  })
 
-    app.get('/aluno/:id', async (req, res) => {
-        const { id } = req.params
+    app.post('/alunos', async (req,res) =>{
         try {
-            if(parseInt(id)){
-                const resposta = await alunosDB.getIdAlunos(id)
-                if (resposta) {
-                    res.status(200).json({
-                        result: resposta,
-                        count: resposta.length
+            const body = req.body
+            const newAluno = new Alunos (body.nome, body.cpf, body.endereco, body.numero, body.bairro, body.cidade, body.estado, body.telefone, body.email, body.idade, body.plano)
+
+            const resp = await alunosDB.insertAlunos(newAluno)
+            res.json(resp)
+            }
+        catch (error) {
+            res.json ({
+            "msg": error.message,
+            "error": true
+      })
+    }
+  })
+
+  app.get('/alunos/:id', async (req, res) => {
+            const id = req.params.id
+        try {
+            const resposta = await alunosDB.getById(id)
+            res.json(resposta)
+        }
+        catch (error){
+            res.status(404).json(error)
+    }
+   })
+
+    app.put('/alunos/:id', async (req, res) => {
+        const id = req.params.id
+        const body = req.body
+
+        try{
+            const get = await alunosDB.getById(id)
+            const update = get.req[0]
+
+            if (update) {
+                const updatedAlunos = new Alunos (body.nome || update.NAME, body.cpf || update.CPF, body.endereco || update.ENDERECO, body.numero || update.NUMERO, body.bairro || update.BAIRRO, body.cidade || update.CIDADE, body.estado || update.ESTADO, body.telefone || update.TELEFONE, body.email || update.EMAIL, body.idade || update.IDADE, body.plano || update.PLANO)
+
+                const resposta = await alunosDB.updateAlunos(id, updatedAlunos)
+                res.json(resposta)
+
+            } else {
+                res.json ({
+                    "msg": `Aluno com id ${id} não existe`,
+                    "error": true
                 })
             }
-            else{
-                throw new Error("Nenhum aluno encontrado.")
-            }
         }
-        else{
-            throw new Error("É esperado um ID do tipo inteiro.")
-            }
-        }
-        catch(erro){
-            res.status(400).json({
-                message: erro.message,
-                error: true
-            })
-    }
-})
-
-    app.post('/aluno', async (req,res) =>{
-        const {nome, cpf, endereco, numero, bairro, cidade, estado, telefone, email, idade, plano} = req.body
-        const newAluno = new Alunos (nome, cpf, endereco, numero, bairro, cidade, estado, telefone, email, idade, plano)
-
-        try{
-            await alunosDB.postAluno(newAluno)
-            res.status(201).json({
-                message: "Aluno inserido com sucesso",
-                error: false
-            })
-        }
-        catch{
-            res.status(400).json({
-                message: "Erro ao inserir aluno.",
-                serverLog: erro.message,
-                error: true
-            })
+        
+        catch(error){
+            res.json ({
+                "msg": error.message,
+                "error": true
+            })       
         }
     })
 
-
-    app.put('/aluno/:id', async (req, res) => {
-        const {nome, cpf, endereco, numero, bairro, cidade, estado, telefone, email, idade, plano} = req.body
-        const {id} = req.params
-        try{
-            await alunosDB.putAluno(nome, cpf, endereco, numero, bairro, cidade, estado, telefone, email, idade, plano)
-            res.status(200).json({
-                message: `Aluno com id: ${id}, foi atualizado.`,
-                error: false
-            })
-        }
-        catch(erro){
-            res.status(400).json({
-                message: "Erro ao atualizar o aluno, verifique se os campos passados são válidos.",
-                serverLog: erro.message,
-                error: true
-            })
-        }
-    })
-
-    app.delete('/aluno/:id', async (req, res) => {
-        const {id} = req.params
+    app.delete('/alunos/:id', async (req, res) => {
+        const id = parseInt(req.params.id)
         try {
-            await alunosDB.deleteAluno(id)
-            res.status(200).json({
-                message: `Aluno com id: ${id}, foi deletado`,
-                error: false
-            })
+            const resposta = await alunosDB.deleteAlunos(id)
+            res.json(resposta)
         }
-        catch (erro) {
-            res.status(400).json ({
-                message: "Erro ao deletar aluno",
-                serverLog: erro.message,
-                error: true
+        catch (error) {
+            res.status(404).json ({
+                "msg": error.message,
+                "error": true
             })
         }
     })
 }
+
+module.exports = expAlunos
